e62d7c058ece267ca5d7d1b8476af883b3aad255
Jn W <a8118199@gmail.com>
2022-05-10 20:54:30 +0900

    rebuild

---
+++ crowdfunding/src/main/java/romance_in_the_room/crowdfunding/CrowdfundingApplication.java
@@ -0,0 +1,13 @@
+package romance_in_the_room.crowdfunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdfundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdfundingApplication.class, args);
+	}
+
+}
---
+++ crowdfunding/src/test/java/romance_in_the_room/crowdfunding/CrowdfundingApplicationTests.java
@@ -0,0 +1,13 @@
+package romance_in_the_room.crowdfunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
