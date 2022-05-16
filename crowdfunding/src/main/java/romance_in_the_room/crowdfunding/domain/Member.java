b53a359f91ff0861242deef0c4762305748b6931
Jn W <a8118199@gmail.com>
2022-05-16 21:58:08 +0900

    연관관계 정리
    //멤버 입장에서 One, 프로젝트 입장에선 Many이기 때문에 OneToMany
    //한 프로젝트의 소유주는 멤버 한명, 하지만 멤버는 여러가지의 프로젝트를 소유할 수 있다

--- crowdfunding/src/main/java/romance_in_the_room/crowdfunding/domain/Member.java
+++ crowdfunding/src/main/java/romance_in_the_room/crowdfunding/domain/Member.java
@@ -2,15 +2,19 @@ package romance_in_the_room.crowdfunding.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import romance_in_the_room.crowdfunding.domain.project.Project;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Entity
 @Getter @Setter
+//멤버 입장에서 One, 프로젝트 입장에선 Many이기 때문에 OneToMany
+//한 프로젝트의 소유주는 멤버 한명, 하지만 멤버는 여러가지의 프로젝트를 소유할 수 있다
 public class Member {
 
     @Id @GeneratedValue
@@ -25,9 +29,11 @@ public class Member {
     @Email
     private String email;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
+    private LocalDateTime createdDate;
 
     @OneToMany(mappedBy = "member")
     private List<Funding> fundings = new ArrayList<>();
+
+    @OneToMany
+    private List<Project> projects = new ArrayList<>();
 }
--- crowdfunding/src/main/java/romance_in_the_room/crowdfunding/domain/project/Project.java
+++ crowdfunding/src/main/java/romance_in_the_room/crowdfunding/domain/project/Project.java
@@ -1,6 +1,7 @@
 package romance_in_the_room.crowdfunding.domain.project;
 
 import lombok.Getter;
+import romance_in_the_room.crowdfunding.domain.Member;
 
 import javax.persistence.*;
 
@@ -11,6 +12,9 @@ public class Project {
     @Id @GeneratedValue
     @Column(name = "project_id")
     private Long id;
+
+    @ManyToOne
+    private Member member;
 
     private String projectName;
 
