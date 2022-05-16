7a24ba07b750aa06def012412282229c0133a76f
Jn W <a8118199@gmail.com>
2022-05-16 22:01:26 +0900

    연관관계 정리2
    @JoinColumn

--- crowdfunding/src/main/java/romance_in_the_room/crowdfunding/domain/project/Project.java
+++ crowdfunding/src/main/java/romance_in_the_room/crowdfunding/domain/project/Project.java
@@ -13,7 +13,8 @@ public class Project {
     @Column(name = "project_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
 
     private String projectName;
