c1529df7fa96d00abda58c5810a95356f45a18bf
Jn W <a8118199@gmail.com>
2022-05-21 14:37:51 +0900

    yml 파일 수정
    디비 패스워드, 유저네임

---
+++ crowdfunding/src/main/java/romance_in_the_room/crowdfunding/controller/MemberController.java
@@ -0,0 +1,4 @@
+package romance_in_the_room.crowdfunding.controller;
+
+public class MemberController {
+}
---
+++ crowdfunding/src/test/java/romance_in_the_room/crowdfunding/MemberTest.java
@@ -0,0 +1,13 @@
+package romance_in_the_room.crowdfunding;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class MemberTest {
+
+
+    void createMember() {
+
+    }
+}
