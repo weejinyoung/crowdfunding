4a0a04bed2e66c1cd3064a68bbe47fba1c76de63
Jn W <a8118199@gmail.com>
2022-06-01 18:18:27 +0900

    펀딩과정
    controller - service - repository

--- crowdfunding/src/main/java/romance_in_the_room/crowdfunding/controller/FundingController.java
+++ crowdfunding/src/main/java/romance_in_the_room/crowdfunding/controller/FundingController.java
@@ -3,9 +3,13 @@ package romance_in_the_room.crowdfunding.controller;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import romance_in_the_room.crowdfunding.domain.funding.Funding;
 import romance_in_the_room.crowdfunding.service.FundingService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,13 +18,21 @@ public class FundingController {
     private final FundingService fundingService;
 
     @PostMapping("/crowdfunding/funding")
-    public CreateFundingResponse createFundingRequest(Long id) {
-        return new CreateFundingResponse(id);
+    public PostFundingResponse createFundingRequest(@PathVariable @Valid PostFundingRequest request) {
+        Long fundingId = fundingService.funding(request.getMemberId(), request.getProjectId());
+        return new PostFundingResponse(fundingId);
     }
 
     @Data
     @AllArgsConstructor
-    static class CreateFundingResponse {
+    static class PostFundingResponse {
         private Long id;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class PostFundingRequest {
+        private Long memberId;
+        private Long projectId;
     }
 }
