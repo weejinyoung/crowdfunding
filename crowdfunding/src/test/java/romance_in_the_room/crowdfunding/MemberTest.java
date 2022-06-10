1c95537cc68ac1c6347d8d4d5f154f99f2225f39
Jn W <a8118199@gmail.com>
2022-06-10 23:50:37 +0900

    MemberTest 생성
    create

--- crowdfunding/src/test/java/romance_in_the_room/crowdfunding/MemberTest.java
+++ crowdfunding/src/test/java/romance_in_the_room/crowdfunding/MemberTest.java
@@ -1,13 +1,26 @@
 package romance_in_the_room.crowdfunding;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import romance_in_the_room.crowdfunding.domain.member.Member;
+import romance_in_the_room.crowdfunding.service.MemberService;
 
 
 @SpringBootTest
 public class MemberTest {
 
+    @Autowired
+    private MemberService memberService;
 
+    @Test
+    @Transactional
     void createMember() {
-
+        Member member1 = Member.createMember("a8118199", "004637", "wee", "a8118199@gmail.com");
+        Long id = memberService.joinMember(member1);
+        Member member2 = memberService.findMember(id);
+        Assertions.assertThat(member1).isEqualTo(member2);
     }
 }
