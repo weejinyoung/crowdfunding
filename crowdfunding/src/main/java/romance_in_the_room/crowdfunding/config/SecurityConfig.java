68b52d5ec80fd706fd8d842bd4f58828f5d68193
Jn W <a8118199@gmail.com>
2022-06-25 23:59:08 +0900

    SecurityConfig 클래스 생성

--- crowdfunding/src/main/java/romance_in_the_room/crowdfunding/config/SecurityConfig.java
+++ crowdfunding/src/main/java/romance_in_the_room/crowdfunding/config/SecurityConfig.java
@@ -1,6 +1,27 @@
 package romance_in_the_room.crowdfunding.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import romance_in_the_room.crowdfunding.service.MemberService;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private final MemberService memberService;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
